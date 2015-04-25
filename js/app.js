d6286173119f37d9937ac17d818abe3a393e7871
TheHighPriestess <elizabeth.hatleli@gmail.com>
2015-04-25 23:01:43 +0300

    added style buttons

--- js/app.js
+++ js/app.js
@@ -4,7 +4,18 @@ $(".togglebutton").click(function(){
 		$("#colorswitch").removeAttr("href");
 	}
 	else $("#colorswitch").attr("href", "css/newcolors.css");
-	});
+	});	 
 
-	 
+$(".styleone").click(function(){
+	$("#colorswitch").attr("href","css/styleone.css");
+});
+
+$(".styletwo").click(function(){
+	$("#colorswitch").attr("href","css/styletwo.css");
+});
+
+$(".stylethree").click(function(){
+	$("#colorswitch").attr("href","css/stylethree.css");
+});
+
 });
